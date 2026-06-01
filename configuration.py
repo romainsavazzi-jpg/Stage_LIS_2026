798b9040789a8089e6ea57f49c718f626fe6034e
romainsavazzi-jpg <romain.savazzi@gmail.com>
2026-06-01 14:08:57 +0200

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -8,8 +8,4 @@ hauteur = info.current_h
 size = largeur, hauteur
 screen = pygame.display.set_mode(size) 
 FPS = 60
-
-vitesse_joueur = 4
-Taille_joueur = 16
-Couleur_joueur = (80, 140, 255)
 Couleur_fond = (30, 30, 30)
