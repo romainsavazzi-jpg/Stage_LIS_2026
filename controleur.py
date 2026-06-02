f3cb07fcbe30ac1a640e1b59c5b8ddd36c89e819
AntoNa1n <antonin.aguado@gmail.com>
2026-06-02 09:55:17 +0200

    v1 du controleur

--- controleur.py
+++ controleur.py
@@ -17,12 +17,16 @@ class Controleur:
             bord_gauche = joueur.x - joueur.taille
             bord_haut = joueur.y - joueur.taille
             bord_bas = joueur.y + joueur.taille
-            if bord_droit >= largeur and dx == 1:
+            if bord_droit + facteur >= largeur and dx == 1:
+                joueur.tp_bord(largeur - bord_droit, "horizontal")
                 dx = 0
-            if bord_gauche <= 0 and dx == -1:
+            if bord_gauche - facteur <= 0 and dx == -1:
+                joueur.tp_bord(0 - bord_gauche, "horizontal")
                 dx = 0
-            if bord_haut <= 0 and dy == -1:
+            if bord_haut - facteur <= 0 and dy == -1:
+                joueur.tp_bord(0 - bord_haut, "vertical")
                 dy = 0
-            if bord_bas >= hauteur and dy == 1:
+            if bord_bas + facteur >= hauteur and dy == 1:
+                joueur.tp_bord(hauteur - bord_bas, "vertical")
                 dy = 0
             joueur.bouger_fleche(dx, dy, facteur)
