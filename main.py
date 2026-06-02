55fff9f453a12d4e60d3496c026345802e120968
romainsavazzi-jpg <romain.savazzi@gmail.com>
2026-06-02 10:56:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-from modele import Joueur
+from modele import Joueur, Carte, Obstacle_rect
 from controleur import Controleur
 from vue import Vue
 from configuration import largeur, hauteur
+import random
 
 # Instanciation dans le bon ordre
 joueur = Joueur(largeur // 2, hauteur // 2)
 controleur = Controleur()
-vue = Vue(joueur, controleur)
+carte = Carte()
+carte.ajouter(joueur)
+for i in range(17):
+    carte.ajouter(
+        Obstacle_rect(
+            x=random.randint(0, largeur - 100),
+            y=random.randint(0, hauteur - 100),
+            largeur=random.randint(20, 150),
+            hauteur=random.randint(20, 150),
+            couleur=(
+                random.randint(0, 255),
+                random.randint(0, 255),
+                random.randint(0, 255),
+            ),
+        )
+    )
+vue = Vue(joueur, controleur, carte)
 
 vue.run()
+
