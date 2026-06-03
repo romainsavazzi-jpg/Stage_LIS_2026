d1ee08dd0ef787ec7e82f2def1ef68208207aff4
AntoNa1n <antonin.aguado@gmail.com>
2026-06-03 07:53:08 +0200

    Correction des tests

--- test_fonctions.py
+++ test_fonctions.py
@@ -7,12 +7,12 @@ def test_mouvement_bord():
     jim = Joueur(0, 0)
     control = Controleur()
     control.gerer_fleches(jim, -1, -1)
-    assert jim.x == jim.y == 0
+    assert jim.x == jim.y == jim.taille
     control.gerer_fleches(jim, -1, 0)
     control.gerer_fleches(jim, 0, -1)
-    assert jim.x == jim.y == 0
+    assert jim.x == jim.y == jim.taille
     control.gerer_fleches(jim, 1, 1)
-    assert jim.x == jim.y == jim.vitesse / math.sqrt(2)
+    assert jim.x == jim.y == jim.taille + jim.vitesse / math.sqrt(2)
     temp = jim.y
     control.gerer_fleches(jim, 0, 1)
     assert jim.y == temp + jim.vitesse
