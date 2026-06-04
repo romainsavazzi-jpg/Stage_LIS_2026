079cfbe6c7651b4a869cfd72883971c728416f9e
AntoNa1n <antonin.aguado@gmail.com>
2026-06-04 13:09:59 +0200

    Ajout d'une grille et de points
    essai de merge le Toml et la grille

--- test_fonctions.py
+++ test_fonctions.py
@@ -6,18 +6,15 @@ import math
 def test_mouvement_bord():
     jim = modele.Joueur(0, 0)
     control = controleur.Controleur()
-    model = modele.Objets_jeu()
-    model.ajouter_joueur(jim)
-    control.attacher_modele(model)
-    control.gerer_deplacement(-1, -1)
+    control.gerer_deplacement(jim, -1, -1)
     assert jim.x == jim.y == jim.taille
-    control.gerer_deplacement(-1, 0)
-    control.gerer_deplacement(0, -1)
+    control.gerer_deplacement(jim, -1, 0)
+    control.gerer_deplacement(jim, 0, -1)
     assert jim.x == jim.y == jim.taille
-    control.gerer_deplacement(1, 1)
+    control.gerer_deplacement(jim, 1, 1)
     assert jim.x == jim.y == jim.taille + jim.vitesse / math.sqrt(2)
     temp = jim.y
-    control.gerer_deplacement(0, 1)
+    control.gerer_deplacement(jim, 0, 1)
     assert jim.y == temp + jim.vitesse
 
 
@@ -31,36 +28,10 @@ def test_mouvement():
 def test_mouvement_click_souris():
     john = modele.Joueur(0, 0)
     controle = controleur.Controleur()
-    model = modele.Objets_jeu()
-    model.ajouter_joueur(john)
-    controle.attacher_modele(model)
     coordonnees = 200
     controle.cible_souris = (coordonnees, coordonnees)
     while controle.cible_souris is not None:
-        controle.deplacer_vers_cible()
+        controle.deplacer_vers_cible(john)
     print(john.x)
-    assert john.x > coordonnees - (john.vitesse // 2 + 1) and john.x < coordonnees + (
-        john.vitesse // 2 + 1
-    )
-    assert john.y > coordonnees - (john.vitesse // 2 + 1) and john.y < coordonnees + (
-        john.vitesse // 2 + 1
-    )
-
-
-def test_ajout_objet():
-    Liste_objet = modele.Objets_jeu()
-    jimmy = modele.Joueur(0, 0)
-    kouign_amann = modele.Obstacle_rect
-    Liste_objet.ajouter_joueur(jimmy)
-    Liste_objet.ajouter_obstacle(kouign_amann)
-    assert Liste_objet.liste_joueurs[0] == jimmy
-    assert Liste_objet.liste_obstacles[0] == kouign_amann
-
-
-def test_recup_joueur():
-    johnny = modele.Joueur(0, 0)
-    cave = modele.Objets_jeu()
-    cave.ajouter_joueur(johnny)
-    cave.ajouter_joueur(johnny)
-    kidnappé = cave.get_joueur(1)
-    assert kidnappé == johnny
+    assert john.x > coordonnees - (john.vitesse // 2 + 1) and john.x < coordonnees + (john.vitesse // 2 + 1)
+    assert john.y > coordonnees - (john.vitesse // 2 + 1) and john.y < coordonnees + (john.vitesse // 2 + 1)
