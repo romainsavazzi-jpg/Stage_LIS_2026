2d4dab72890dad724caa4db2716d32973236e451
romainsavazzi-jpg <romain.savazzi@gmail.com>
2026-06-16 16:08:11 +0200

    debut fonction rect - cercle

--- modele/modele.py
+++ modele/modele.py
@@ -44,7 +44,9 @@ class Collisions:
                 return True
 
     def collision_cercle_rect(self, rect, dx, dy, facteur):
-        pass
+        carre_autour_du_cercle = Obstacle_rect(self.x - self.taille, self.y - self.taille / 2, 2 * self.taille, 2 * self.taille)
+        if not carre_autour_du_cercle.collision_rect_rect(rect, dx, dy, facteur):
+            return False
 
 
 class Joueur(Collisions):
@@ -81,7 +83,7 @@ class Joueur(Collisions):
         self.vitesse += increment
 
 
-class Obstacle:
+class Obstacle(Collisions):
     def __init__(self, x, y):
         self.x = x
         self.y = y
